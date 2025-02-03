2ce5c0fd1359d9fb873668d185cd51bb5f7aae46
GabrielSilva87 <gabri2012mi@gmail.com>
2025-02-03 17:37:38 -0300

    Update userr.php

--- core/user/userr.php
+++ core/user/userr.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace App\Models;
+
+use Core\Model;
+
+class User extends Model {
+    public function getAllUsers() {
+        return $this->fetchAll("SELECT * FROM users");
+    }
+
+    public function getUser ById($id) {
+        return $this->fetch("SELECT * FROM users WHERE id = ?", [$id]);
+    }
+}
